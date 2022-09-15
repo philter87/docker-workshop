75d480333441780a41d6df81a6e41353091a61b9
Philip Christiansen <philipchristiansen@gmail.com>
2022-09-15 20:38:27 +0200

    first commit

---
+++ parameters/src/test/java/dk/mjolner/workshop/docker/parameters/ParametersApplicationTests.java
@@ -0,0 +1,13 @@
+package dk.mjolner.workshop.docker.parameters;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParametersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
