af37c33ca1667674b6b74eba7696795b486ac94c
Philip Hjorth Christiansen <pch@mjolner.dk>
2022-10-24 20:58:57 +0200

    Exercise text changed

--- c-docker-vs-code/app/src/main/java/dk/mjolner/workshop/docker/parameters/Exercise.java
+++ c-docker-vs-code/app/src/main/java/dk/mjolner/workshop/docker/parameters/Exercise.java
@@ -8,30 +8,24 @@ import org.springframework.web.client.RestTemplate;
 @RestController
 public class Exercise {
     private final RestTemplate restTemplate = new RestTemplate();
+    public static final String BaseUrl = "http://localhost:8080";
+    public static final String UserDisplayName = "YourName";
 
     @GetMapping(value = "exercise")
     public boolean exercise() {
-        var baseUrl = "http://localhost:8080";
-        var displayName = "YourName";
-
-        // Get the ships
-        var ships = restTemplate.getForObject(baseUrl + "/game/score", Ship[].class);
-
         var x = 5;
         var y = 5;
 
-        // ** MOVING YOUR SHIP **
-        // You can move the location of your ship. Every move gives you 1 point. You can only move to locations next to your current location.
-        // You may need to find your ship in the ships-array to know your current location. You can only move once every second.
-        restTemplate.getForObject(baseUrl + "/game/move?x=" + x + "&y=" + y + "&displayName=" + displayName, String.class);
+        // MOVEMENT OF SHIP. Allowed once every second. Gives 1 point
+        // restTemplate.getForObject(BaseUrl + "/game/move?x=" + x + "&y=" + y + "&displayName=" + UserDisplayName, String.class);
 
-        // ** SHOOTING WITH YOUR SHIP **
-        // You can shoot other ships in the game. You get 5 points for hitting an opponent, but you can only do this every 5 seconds. 
-        // There are no penalties for missing or shooting yourself. You get 5 points per hit.
-        // 
-        // restTemplate.getForObject(baseUrl + "/game/shoot?x=" + x + "&y=" + y + "&displayName=" + displayName, String.class);
+        // SHOOTING WITH SHIP. Allowed once every five second. Gives 5 points.
+        // restTemplate.getForObject(BaseUrl + "/game/shoot?x=" + x + "&y=" + y + "&displayName=" + UserDisplayName, String.class);
 
-        return false;
+        // Get all ships. You can use this
+        // var ships = restTemplate.getForObject(BaseUrl + "/game/score", Ship[].class);
+
+        return true;
     }
 
 }
--- c-docker-vs-code/app/src/main/java/dk/mjolner/workshop/docker/parameters/TaskCController.java
+++ c-docker-vs-code/app/src/main/java/dk/mjolner/workshop/docker/parameters/TaskCController.java
@@ -1,5 +1,6 @@
 package dk.mjolner.workshop.docker.parameters;
 
+import dk.mjolner.workshop.docker.parameters.game.Ship;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,10 +10,14 @@ import java.io.IOException;
 
 import javax.servlet.http.HttpServletRequest;
 
+import static dk.mjolner.workshop.docker.parameters.Exercise.BaseUrl;
+import static dk.mjolner.workshop.docker.parameters.Exercise.UserDisplayName;
 import static j2html.TagCreator.*;
 
 @RestController
 public class TaskCController {
+    private final RestTemplate restTemplate = new RestTemplate();
+    public static final String RemoteUrl = "http://20.223.122.66:8080";
 
     @GetMapping(value = "c", produces = MediaType.TEXT_HTML_VALUE)
     public String cPage(HttpServletRequest req) throws IOException {
@@ -31,8 +36,10 @@ public class TaskCController {
                         Utils.bullet("Open VS code, press 'F1' and then select 'Dev Container: Open folder in container ' (or Remote Container: Open folder in container). Select the folder: 'c-docker-vs-code/app'. Now you need to select several options: 'From a predefined container configuration', 'Java', '17-bullseye', 'node lts', 'Install Gradle', '0 selected'. Now you need to wait while for dev container to start up. It will also ask you to install an extension and you should accept this - it is only installed within the container."),
                         Utils.bullet("While you wait, you can take a look at the folder called '.devcontainer' which was created by VS code. It contains a Dockerfile and a devcontainer.json file. See if you can figure out what it means"),
                         Utils.bullet("Go to the class 'DockerWorkshopApplication' and see if you can run the application. The port is automatically published to 8080, so you can view result here: 'http://localhost:8080'. (If you have issues running java it might help to press F1 --> 'Java: Clean Java Language Server Workspace')", isRunningOnPort8080(req)),
-                        Utils.bullet("Open the class called 'Exercise', where you will find the 'exercise()'-method. This is executed whenever you call 'http://localhost:8080/exercise' (or when this page is refreshed). Start by return 'true' instead of false. This will make this turn green", isExerciseEndpointImplemented()),
-                        Utils.bullet("Now we will play 'The Radar Game' together. You need to write some more code to win. You should change 'baseUrl' to 'http://20.223.122.66:8080' instead of 'http://localhost:8080'. Rerun the application and you should then appear in the shared radar game. Adjust the alogrithm to win the game.")
+                        Utils.bullet("Open the class called 'Exercise', where you will find the 'exercise()'-method. This is executed whenever you call 'http://localhost:8080/exercise' (or when this page is refreshed). The method should return 'true' instead of 'false'", isExerciseEndpointImplemented()),
+                        Utils.bullet("Now you should try to make your first 'move' by modifying the 'exercise'-method again. You should uncomment the line after 'MOVEMENT OF SHIP' and change the 'UserDisplayName' to your actual name. When then endpoint 'localhost:8080/exercise' is called, it will also call 'localhost:8080/game/move' which will add you to the game. It will make this turn green, and you can also see your score if you visit http://localhost:8080/game/score.", isPlayerAddedToTheGame()),
+                        Utils.bullet("We have played 'The Radar Game' locally in the previous steps. Now you can play against your colleagues if you change the BaseUrl to '" + RemoteUrl + "' in the class 'Exercise'. Rerun the application and refresh this page.", isPlayerAddedInRemoteGame()),
+                        Utils.bullet("To win the game you need to change the algorithm in the 'exercise()'-method. You should automatically move every second and shoot every 5 seconds.")
                 ),
                 div(a("Go back").withHref("/")),
                 div(a("Kill the application").withHref("/kill"))
@@ -44,9 +51,30 @@ public class TaskCController {
     }
 
     public boolean isExerciseEndpointImplemented(){
-        var restTemplate = new RestTemplate();
-        var result =  restTemplate.getForObject("http://localhost:8080/exercise", Boolean.class);
-        return (boolean) result;
-    
+        return restTemplate.getForObject("http://localhost:8080/exercise", Boolean.class);
+    }
+
+    public boolean isPlayerAddedInRemoteGame(){
+        var ships = restTemplate.getForObject(RemoteUrl + "/game/score", Ship[].class);
+        for (Ship ship : ships) {
+            if(ship.getDisplayName().equals(UserDisplayName)){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean isPlayerAddedToTheGame(){
+        if(BaseUrl.equals(RemoteUrl)) {
+            return true;
+        }
+
+        var ships = restTemplate.getForObject(BaseUrl + "/game/score", Ship[].class);
+        for (Ship ship : ships) {
+            if(!ship.getDisplayName().equals("YourName") && !ship.getDisplayName().equals("AI")){
+                return true;
+            }
+        }
+        return false;
     }
 }
